cb8409dd7cceab607da653727f83c202427fd41f
Jason Parkin <jay-parkin91@hotmail.com>
2024-10-04 16:55:18 +1000

    feat: theme toggler implemented

---
+++ src/contexts/ThemeContextProvider.jsx
@@ -0,0 +1,33 @@
+import { useEffect, useContext, createContext, useState } from "react";
+
+export const ThemeContext = createContext("dark");
+
+export function useThemeContext() {
+  return useContext(ThemeContext);
+}
+
+export function ThemeContextProvider({ children }) {
+  let [currentTheme, setCurrentTheme] = useState("dark");
+
+  useEffect(() => {
+    document.documentElement.style.setProperty("color-scheme", currentTheme);
+  }, [currentTheme]);
+
+  const toggleTheme = () => {
+    if (currentTheme == "dark") {
+      setCurrentTheme("light");
+    } else {
+      setCurrentTheme("dark");
+    }
+  };
+
+  const setToSystem = () => {
+    setCurrentTheme("light dark");
+  };
+
+  return (
+    <ThemeContext.Provider value={[currentTheme, toggleTheme, setToSystem]}>
+      {children}
+    </ThemeContext.Provider>
+  );
+}
