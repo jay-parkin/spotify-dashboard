5bd63e5e5f5d07dce75fd3b82989ce5b8bd9ab2b
Jason Parkin <jay-parkin91@hotmail.com>
2024-10-04 17:14:12 +1000

    feat: sign in with spotify

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import "./App.css";
+import { useSpotifyAuthContext } from "./contexts/SpotifyAuthProvider";
 import { useThemeContext } from "./contexts/ThemeContextProvider";
 
 function App() {
-  const [currentTheme, toggleTheme] = useThemeContext();
+  const [currentTheme, toggleTheme, setToSystem] = useThemeContext();
+  const { redirectToAuthCodeFlow } = useSpotifyAuthContext();
 
   return (
     <>
-      <button onClick={toggleTheme}>Toggle Theme</button>
+      <button onClick={toggleTheme}>Toggle theme</button>
+      <button onClick={setToSystem}>Set to system theme</button>
+      <button onClick={redirectToAuthCodeFlow}>Sign in via Spotify</button>
     </>
   );
 }
